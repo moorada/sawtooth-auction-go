4214160149d414fff89b0b7de180717564c039a7
moorada <a.robustella1@studenti.uniba.it>
2021-11-29 11:05:58 +0100

    test.sh

--- src/sawtooth_auction_client/test.sh
+++ src/sawtooth_auction_client/test.sh
@@ -4,29 +4,27 @@
 #
 #
 #
-
-go build
-./sawtooth_auction_client addItem "11111" "T-shirt" "nice" "a" "v"
+sawtooth_auction_client addItem "11111" "T-shirt" "nice" "a" "v"
 sleep 1
-./sawtooth_auction_client addItem "22222" "Jacket" "nice" "a" "v"
+sawtooth_auction_client addItem "22222" "Jacket" "nice" "a" "v"
 sleep 1
-./sawtooth_auction_client addItem "33333" "Bike" "nice" "a" "v"
+sawtooth_auction_client addItem "33333" "Bike" "nice" "a" "v"
 sleep 1
-./sawtooth_auction_client addItem "44444" "Car" "nice" "a" "v"
+sawtooth_auction_client addItem "44444" "Car" "nice" "a" "v"
 sleep 4
-./sawtooth_auction_client placeBid "11111" "12" "Ted" "152" "timestamp"
+sawtooth_auction_client placeBid "11111" "12" "Ted" "152" "timestamp"
 sleep 1
-./sawtooth_auction_client placeBid "11111" "11"  "Joe" "150" "timestamp"
+sawtooth_auction_client placeBid "11111" "11"  "Joe" "150" "timestamp"
 sleep 1
-./sawtooth_auction_client placeBid "22222" "21" "Oliver" "19" "timestamp"
+sawtooth_auction_client placeBid "22222" "21" "Oliver" "19" "timestamp"
 sleep 1
-./sawtooth_auction_client placeBid "22222" "22" "Elon" "18" "timestamp"
+sawtooth_auction_client placeBid "22222" "22" "Elon" "18" "timestamp"
 sleep 1
-./sawtooth_auction_client placeBid "22222" "23" "Mary" "15" "timestamp"
+sawtooth_auction_client placeBid "22222" "23" "Mary" "15" "timestamp"
 sleep 1
-./sawtooth_auction_client placeBid "44444" "41" "Mary" "1232" "timestamp"
+sawtooth_auction_client placeBid "44444" "41" "Mary" "1232" "timestamp"
 sleep 1
-./sawtooth_auction_client placeBid "33333" "31" "Ted" "550" "timestamp"
+sawtooth_auction_client placeBid "33333" "31" "Ted" "550" "timestamp"
 sleep 1
 
 ./sawtooth_auction_client show "11111"
